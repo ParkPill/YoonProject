299221cf0b1ca760d7f010d96ea091af5bf96553
ParkPill <rightguy22@gmail.com>
2020-03-31 15:44:29 +0900

    initial

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+
+{
+    public float Speed = 10;
+    public float LifeTime = 1;
+    private float _time;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _time += Time.deltaTime;
+        if (_time > LifeTime)
+        {
+            Destroy(this.gameObject);
+        }
+        transform.Translate(transform.forward * Speed);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Angle;
+    public float Speed = 2;
+    public float Sensitive = 2;
+    public GameObject Missile;
+    public float CoolTime = 0.2f;
+    private float _coolTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _coolTime += Time.deltaTime;
+        if (_coolTime > CoolTime)
+        {
+            float y = transform.rotation.y;
+            y += Input.GetAxis("Horizontal") * Sensitive * Time.deltaTime;
+            //transform.rotation = new Quaternion(0, y, 0, 1);
+            transform.Rotate(0, y, 0, Space.Self);
+
+            if (Input.GetKey(KeyCode.H))
+            {
+                GameObject ms = Instantiate(Missile);
+                ms.transform.position = this.transform.position;
+                ms.transform.rotation = this.transform.rotation;
+                
+            }
+            _coolTime = 0;
+        }
+    }
+}
